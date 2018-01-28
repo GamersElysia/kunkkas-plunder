ec71d209a316e95a848ef1c6753ef97b377d3cc6
Alex Barrett <vcs@alexbarrett.com>
2018-01-28 15:13:31 +0000

    Add pygame package and create empty window

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+size = 960,720
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Kunkka's Plunder")
+
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (0, 0, 128), (0, 0, 100, 100))
+    pygame.display.flip()
