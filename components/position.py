7b5fe8a79c10fc40c556a621ae75ddcfad461bc3
Alex Barrett <vcs@alexbarrett.com>
2018-01-31 01:02:37 +0000

    Implement __repr__ for Position component

--- components/position.py
+++ components/position.py
@@ -3,3 +3,6 @@ class Position:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+
+    def __repr__(self):
+        return "{}(x={}, y={})".format(self.__class__.__name__, self.x, self.y)
